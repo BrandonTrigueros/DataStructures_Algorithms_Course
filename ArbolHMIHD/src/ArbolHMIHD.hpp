e3477ede2fdc564bc67f35600835f9c369295a7d
lester.pereira <lester.pereira@ucr.ac.cr>
2023-10-12 23:19:55 -0600

    Avance arbol

--- ArbolHMIHD/src/ArbolHMIHD.hpp
+++ ArbolHMIHD/src/ArbolHMIHD.hpp
@@ -11,11 +11,27 @@ class ArbolHMIHD
       nodoArbol* hijoMasIzq;
       nodoArbol* hermanoDer;
     };
-    nodoArbol* raiz;
+    nodoArbol* raizArbol;
+    int contadorNodos;
   public:
     ArbolHMIHD();
     ~ArbolHMIHD();
-
+    void crear();
+    void destruir();
+    void destruirPostOrden(nodoArbol* nodo);
+    void vaciar();
+    bool vacio();
+    void ponerRaiz(int etiqueta);
+    int agregarHijo(int etiquetaAgregar, int etiquetaPadre, int posicion);
+    void borrarHoja(int etiqueta);
+    void modificarEtiqueta(int etiqueta, int nuevaEtiqueta);
+    int raiz();
+    int padre(int etiqueta);
+    int hijoMasIzquierdo(int etiqueta);
+    int hermanoDerecho(int etiqueta);
+    int etiqueta(nodoArbol* nodo);
+    int numNodos();
+    int numHijos(int etiqueta);
 };
 
 #endif // ARBOLHMIHD_HPP
