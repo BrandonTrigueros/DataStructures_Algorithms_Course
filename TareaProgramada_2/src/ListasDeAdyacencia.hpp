59272047abe398b30db70eb0d853a5e284f1b13b
Christopher <christopher.acostamadrigal@ucr.ac.cr>
2023-11-10 13:28:54 -0600

    Creacion de archivos y separacion de carpetas para TareaProgramada_2

---
+++ TareaProgramada_2/src/ListasDeAdyacencia.hpp
@@ -0,0 +1,8 @@
+#ifndef LISTAS_DE_ADYACENCIA
+#define LISTAS_DE_ADYACENCIA
+
+class ListasDeAdyacencia {
+
+};
+
+#endif //LISTAS_DE_ADYACENCIA
---
+++ TareaProgramada_2/src/pruebas.cpp
@@ -0,0 +1,5 @@
+#include "ListasDeAdyacencia.hpp"
+
+int main () {
+
+}
