23aee8dedb47047013f66a93a6cc20a73a313071
Christopher <christopher.acostamadrigal@ucr.ac.cr>
2023-11-12 00:15:18 -0600

    Lista de listas mayoria de operadores

--- TareaProgramada_2/src/ListasDeAdyacencia.cpp
+++ TareaProgramada_2/src/ListasDeAdyacencia.cpp
@@ -1,1 +1,138 @@
 #include "ListasDeAdyacencia.hpp"
+#include <iterator>
+
+
+void ListasDeAdyacencia::Iniciar(){
+  this->listaVert = nullptr; 
+  this->cantVert = 0;
+}
+
+void ListasDeAdyacencia::Destruir() {
+  if (!this->Vacio()) {
+    Vaciar();
+  } 
+}
+
+void ListasDeAdyacencia::Vaciar() {
+  Vertice* aux = this->listaVert;
+  while (aux != nullptr) {
+    Aristas* aristaAux = aux->listaAristas;
+    while (aristaAux != nullptr) {
+      Aristas* aristaBorrar = aristaAux;
+      aristaAux = aristaAux->sigArista;
+      this->EliminarArista(aux, aristaBorrar->verticeApuntado);
+    }
+    Vertice* borrar = aux;
+    aux = aux->siguienteVert;
+    this->EliminarVert(borrar); 
+  }
+}
+
+bool ListasDeAdyacencia::Vacio() {
+  if (this->listaVert != nullptr) {
+    return false;
+  }
+  return true;
+}
+
+Vertice* ListasDeAdyacencia::AgregarVert(std::string etiq) {
+  Vertice* toAdd = new Vertice();
+  toAdd->etiqueta = etiq;
+
+  if (this->listaVert == nullptr) {
+    this->listaVert = toAdd;
+  } else { 
+    toAdd->siguienteVert = this->listaVert;
+    this->listaVert = toAdd;
+  }
+
+  return toAdd;
+}
+
+void ListasDeAdyacencia::EliminarVert(Vertice* vert) {
+  Vertice* iterador = this->listaVert;
+  if (vert == iterador) {
+    this->listaVert = iterador->siguienteVert;
+    delete iterador;
+    this->cantVert--;
+    return;
+  }
+
+  while (iterador != nullptr && iterador->siguienteVert != vert) {
+    iterador = iterador->siguienteVert;
+  }
+
+  iterador->siguienteVert = vert->siguienteVert;
+  this->cantVert--;
+  delete vert;
+}
+
+void ListasDeAdyacencia::ModificarEtiqueta(Vertice* vert, std::string etiq) {
+  vert->etiqueta = etiq;
+}
+
+std::string ListasDeAdyacencia::Etiqueta(Vertice* vert) {
+  return vert->etiqueta;
+}
+
+void ListasDeAdyacencia::AgregarArista(Vertice* salida, Vertice* llegada, double peso) {
+  Aristas* aristaAgregar= new Aristas();
+  aristaAgregar->peso = peso;
+  aristaAgregar->verticeApuntado = llegada;
+  aristaAgregar->sigArista = salida->listaAristas;
+
+  salida->listaAristas = aristaAgregar;
+}
+
+void ListasDeAdyacencia::EliminarArista(Vertice* salida, Vertice* llegada) {
+  Aristas* aristaABorrar = salida->listaAristas;  
+  if (aristaABorrar->verticeApuntado == llegada) {
+    salida->listaAristas = aristaABorrar->sigArista;
+    delete aristaABorrar;
+    return;
+  }
+
+  while (aristaABorrar->sigArista->verticeApuntado != llegada) {
+    aristaABorrar = aristaABorrar->sigArista;
+  }
+
+  aristaABorrar->sigArista = aristaABorrar->sigArista->sigArista;
+  delete aristaABorrar->sigArista;
+}
+
+void ListasDeAdyacencia::ModificarPeso(Vertice* salida, Vertice* llegada, double peso) {
+  Aristas* pesoAModificar = salida->listaAristas;
+  while (pesoAModificar->verticeApuntado != llegada) {
+    pesoAModificar = pesoAModificar->sigArista;
+  }
+  pesoAModificar->peso = peso; 
+}
+
+double ListasDeAdyacencia::Peso(Vertice* salida, Vertice* llegada) {
+  Aristas* pesoARetornar= salida->listaAristas;
+  while (pesoARetornar->verticeApuntado != llegada) {
+    pesoARetornar = pesoARetornar->sigArista;
+  }
+  return pesoARetornar->peso;
+}
+
+Vertice* ListasDeAdyacencia::PrimerVertice() {
+  return this->listaVert;
+}
+
+Vertice* ListasDeAdyacencia::SiguienteVertice(Vertice* vert) {
+  return vert->siguienteVert;
+}
+
+Vertice* ListasDeAdyacencia::PrimerVerticeAdyacente(Vertice* primer, Vertice* segundo) {
+  
+}
+
+Vertice* ListasDeAdyacencia::SiguienteVerticeAdyacente(Vertice* vert) {
+  return vert->listaAristas->verticeApuntado;
+}
+
+int64_t ListasDeAdyacencia::NumVertices() {
+  return this->cantVert;
+}
+
--- TareaProgramada_2/src/ListasDeAdyacencia.hpp
+++ TareaProgramada_2/src/ListasDeAdyacencia.hpp
@@ -1,8 +1,48 @@
 #ifndef LISTAS_DE_ADYACENCIA
 #define LISTAS_DE_ADYACENCIA
 
-class ListasDeAdyacencia {
+#include <cstdint>
+#include <string>
 
+#include <iostream>
+
+struct Vertice;
+
+struct Aristas {
+  double peso = 0;
+  Vertice *verticeApuntado = nullptr;
+  Aristas* sigArista = nullptr;
 };
 
-#endif //LISTAS_DE_ADYACENCIA
+struct Vertice {
+  Aristas *listaAristas = nullptr;
+  Vertice *siguienteVert = nullptr;
+  std::string etiqueta = "";
+};
+
+class ListasDeAdyacencia {
+ public:
+  Vertice *listaVert;
+  int64_t cantVert;
+
+ public:
+  void Iniciar();
+  void Destruir();
+  void Vaciar();
+  bool Vacio();
+  Vertice* AgregarVert(std::string etiq);
+  void EliminarVert(Vertice* vert);
+  void ModificarEtiqueta(Vertice* vert, std::string etiq);
+  std::string Etiqueta(Vertice* vert);
+  void AgregarArista(Vertice* salida, Vertice* llegada, double peso);
+  void EliminarArista(Vertice* salida, Vertice* llegada);
+  void ModificarPeso(Vertice* salida, Vertice* llegada, double peso);
+  double Peso(Vertice* salida, Vertice* llegada);
+  Vertice* PrimerVertice();
+  Vertice* SiguienteVertice(Vertice* vert);
+  Vertice* PrimerVerticeAdyacente(Vertice* primer, Vertice* segundo);
+  Vertice* SiguienteVerticeAdyacente(Vertice* vert);
+  int64_t NumVertices();
+};
+
+#endif // LISTAS_DE_ADYACENCIA
